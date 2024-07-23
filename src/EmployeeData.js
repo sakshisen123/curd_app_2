b11a6b2204df447b99b67f98d5d2e7963b95c783
Sakshi Sen <sensakshi50@gmail.com>
2024-07-24 01:48:57 +0530

    Ádded essential files

---
+++ src/EmployeeData.js
@@ -0,0 +1,26 @@
+export const EmployeeData = [
+  {
+    id: 1,
+    firstName: 'Sakshi',
+    lastName: 'Sen',
+    age: 23
+  },
+  {
+    id: 2,
+    firstName: 'Rohit',
+    lastName: 'Jat',
+    age: 22
+  },
+  {
+    id: 3,
+    firstName: 'Amit',
+    lastName: 'Vishwakarma',
+    age: 23
+  },
+  {
+    id: 4,
+    firstName: 'Mukesh',
+    lastName: 'Rao',
+    age: 23
+  }
+]
